94fdb9c0c24bcd7237ac78f357b35360feb1de89
Alex Cipollone <alex.cipollone10@gmail.com>
2023-01-22 22:29:51 -0500

    Committing again

--- 25_clock/src/App.js
+++ 25_clock/src/App.js
@@ -2,6 +2,7 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 import SessionTimer from './components/SessionTimer';
+import SessionBlock from './components/SessionBlock';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowUp, faArrowDown, faPlay, faUndo, faPause } from '@fortawesome/free-solid-svg-icons'
 
@@ -15,65 +16,25 @@ const App = () =>  {
 
   return (
     <> 
-        <div className="row">
-            <div className="col-md-3"></div>
-            <div className="col-md-6">
-                <h1 id="title">25 + 5 Clock</h1>
-                <p id="definition">The Pomodoro Technique is a time management method. It is used to break work into intervals, typically 25 minutes long, separated by short breaks. It was created by Francesco Cirillo.</p>
-                <div id="blocks">
-                    <SessionTimer />
-                    <div id="break-block">
-                        <p id="break-label">Break Length</p>
+      <div className="row">
+        <div className="col-md-3"></div>
+          <div className="col-md-6">
+             <h1 id="title">25 + 5 Clock</h1>
+             <p id="definition">The Pomodoro Technique is a time management method. It is used to break work into intervals, typically 25 minutes long, separated by short breaks. It was created by Francesco Cirillo.</p>
+             <div id="blocks">
+                 <div id="break-block">
+                    <p id="break-label">Break Length</p>
                     <p><i id="break-increment">
                     <FontAwesomeIcon icon={faArrowDown} /></i></p>
-                    </div>
-                </div>
-                <div id="timer-border">
-                    <div id="timer-label">{timerLabel}</div>
-                    <div id="time-left">
-
-                        <audio id="beep" preload="auto" src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"></audio>
-                    </div>
-                </div>
-                <div id="timer-options">
-                    <div id="start_stop"><i className="fa-solid fa-play" onClick={function decrementSession(){
-                          setSessionMinutes(prevState => prevState - 1)
-                          console.log(sessionMinutes)
-                          setSessionSeconds(59);
-                          console.log(sessionSeconds)
-                          while(sessionSeconds > 0) {
-                            console.log(sessionSeconds)
-                          }
-                          }
-                    }>{playPause}</i></div>
-                    <div id="reset"><i className="fas fa-undo"><FontAwesomeIcon icon={faUndo} /></i></div>
-                </div>
-            </div>
+                  </div>
+              </div>
+              <SessionTimer />
             <div className="col-md-3"></div>
         </div>
+      </div>
     </>
   );
 }
 
 export default App;
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
--- 25_clock/src/components/SessionTimer.jsx
+++ 25_clock/src/components/SessionTimer.jsx
@@ -5,7 +5,34 @@ import { faArrowUp, faArrowDown, faPlay, faUndo, faPause } from '@fortawesome/fr
 
 const SessionTimer = () => {
     const [sessionMinutes, setSessionMinutes] = useState(25);
-    const [sessionSeconds, setSessionSeconds] = useState(0);
+    const [sessionSeconds, setSessionSeconds] = useState(60);
+    const [timerLabel, setTimerLabel] = useState("Session");
+    const [isActive, setIsActive] = useState(false);
+    const [playPause, setPlayPause] = useState(<FontAwesomeIcon icon={faPlay} />);
+
+    function toggle() {
+        setIsActive(!isActive);
+    }
+
+    function incrementSessionMinutes() {
+        setSessionMinutes(sessionMinutes + 1);
+    }
+
+    function decrementSessionMinutes() {
+        if(sessionMinutes > 0){
+            setSessionMinutes(sessionMinutes - 1);
+        }
+    }
+
+    useEffect(()=>{
+        let interval = null;
+        if(isActive && sessionSeconds > 0){
+            interval = setInterval(()=>{
+                setSessionSeconds(sessionSeconds => sessionSeconds - 1);
+            },1000);
+        } 
+        return () => clearInterval(interval);
+    }, [isActive, sessionSeconds]);
 
 
     return(
@@ -13,15 +40,24 @@ const SessionTimer = () => {
             <div id="session-block">        
                 <p id="session-label">Session Length</p>
                     <div id="session-counter">
-                    <div className="expand"><i id="session-increment" onClick={
-                        ()=>{
-                            setSessionMinutes(sessionMinutes + 1);
-                        }
-                    }><FontAwesomeIcon icon={faArrowUp} /></i></div>
+                    <div className="expand"><i id="session-increment" onClick={incrementSessionMinutes}>
+                        <FontAwesomeIcon icon={faArrowUp} /></i></div>
                         <div id="session-length" className="expand">{sessionMinutes}:0{sessionSeconds}</div>
-                        <div className="expand"><i id="session-decrement"><FontAwesomeIcon icon={faArrowDown} /></i></div>
+                        <div className="expand"><i id="session-decrement" onClick={decrementSessionMinutes}>
+                        <FontAwesomeIcon icon={faArrowDown} /></i></div>
                     </div>
             </div>
+            <div id="timer-border">
+        <div id="timer-label">{timerLabel}</div>
+        <div id="time-left">{sessionMinutes}:{sessionSeconds}
+          <audio id="beep" preload="auto" src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"></audio>
+        </div>
+       </div>
+        <div id="timer-options">
+            <div id="start_stop"><i className="fa-solid fa-play" onClick={toggle}>
+                {playPause}</i></div>
+            <div id="reset"><i className="fas fa-undo"><FontAwesomeIcon icon={faUndo} /></i></div>
+        </div>
         </>
     )
 }
