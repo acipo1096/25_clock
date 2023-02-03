5f865213b3e49243f6ad86c144a96a7e8dcce6d7
Alex Cipollone <alex.cipollone10@gmail.com>
2023-02-03 11:44:21 -0500

    I think I have a functional timer!

--- 25_clock/src/App.js
+++ 25_clock/src/App.js
@@ -1,17 +1,28 @@
 // import logo from './logo.svg';
 import React, {useState, useEffect} from 'react';
-import './App.css';
+// import './App.css';
 import SessionTimer from './components/SessionTimer';
-import SessionBlock from './components/SessionBlock';
+import BreakTimer from './components/BreakTimer';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowUp, faArrowDown, faPlay, faUndo, faPause } from '@fortawesome/free-solid-svg-icons'
 
 
 const App = () =>  {
 
-  const [breakTime, setBreakTime] = useState(5);
-  const [timerLabel, setTimerLabel] = useState("Session");
-  const [playPause, setPlayPause] = useState(<FontAwesomeIcon icon={faPlay} />);
+  const session = <SessionTimer />;
+
+  // const [sessionMinutes, setSessionMinutes] = useState(1);
+  // const [sessionSeconds, setSessionSeconds] = useState(11);
+  // const [timerLabel, setTimerLabel] = useState("Session");
+  // const [isActive, setIsActive] = useState(false);
+  // const [playPause, setPlayPause] = useState(<FontAwesomeIcon icon={faPlay} />);
+  // const nextSecond = sessionSeconds - 1;
+  // const nextMinute = sessionMinutes - 1;
+  // let timeLeft = document.getElementById('time-left')
+
+  // function swapTimers() {
+  
+  // }
 
 
   return (
@@ -22,13 +33,8 @@ const App = () =>  {
              <h1 id="title">25 + 5 Clock</h1>
              <p id="definition">The Pomodoro Technique is a time management method. It is used to break work into intervals, typically 25 minutes long, separated by short breaks. It was created by Francesco Cirillo.</p>
              <div id="blocks">
-                 <div id="break-block">
-                    <p id="break-label">Break Length</p>
-                    <p><i id="break-increment">
-                    <FontAwesomeIcon icon={faArrowDown} /></i></p>
-                  </div>
-              </div>
               <SessionTimer />
+              </div>
             <div className="col-md-3"></div>
         </div>
       </div>
--- 25_clock/src/components/SessionTimer.jsx
+++ 25_clock/src/components/SessionTimer.jsx
@@ -2,30 +2,65 @@ import React, {useState, useEffect, isValidElement} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowUp, faArrowDown, faPlay, faUndo, faPause } from '@fortawesome/free-solid-svg-icons'
 
-
 const SessionTimer = () => {
-    const [sessionMinutes, setSessionMinutes] = useState(25);
+    const [defaultSession, setDefaultSession] = useState(2)
+    const [sessionMinutes, setSessionMinutes] = useState(2);
     const [sessionSeconds, setSessionSeconds] = useState(60);
     const [timerLabel, setTimerLabel] = useState("Session");
+    const [isBreak, setIsBreak] = useState(false);
     const [isActive, setIsActive] = useState(false);
     const [playPause, setPlayPause] = useState(<FontAwesomeIcon icon={faPlay} />);
+    const [defaultBreak, setDefaultBreak] = useState(1)
+    const [breakMinutes, setBreakMinutes] = useState(1);
+    const [breakSeconds, setBreakSeconds] = useState(60);
+    const nextBreakSecond = breakSeconds - 1;
+    const nextBreakMinute = breakMinutes - 1;
     const nextSecond = sessionSeconds - 1;
     const nextMinute = sessionMinutes - 1;
-    let timeLeft = document.getElementById('time-left')
+    let timeLeft = document.getElementById('time-left');
+    let sessionLength = document.getElementById('session-length');
+    let breakLength = document.getElementById('break-length');
+
 
     function toggle() {
         setIsActive(!isActive);
     }
 
+    function toggleBreak() {
+        setIsBreak(!isBreak);
+        console.log(isBreak)
+    }
+
+
     function incrementSessionMinutes() {
         setSessionMinutes(sessionMinutes + 1);
+        setDefaultSession(defaultSession + 1);
     }
 
     function decrementSessionMinutes() {
         if(sessionMinutes > 0){
             setSessionMinutes(sessionMinutes - 1);
+            setDefaultSession(defaultSession - 1);
         }
     }
+
+    function incrementBreakMinutes() {
+        setBreakMinutes(breakMinutes + 1);
+        setDefaultBreak(defaultBreak + 1);
+    }
+
+    function decrementBreakMinutes() {
+        if(breakMinutes > 0){
+            setBreakMinutes(breakMinutes - 1);
+            setDefaultBreak(defaultBreak - 1);
+        }
+    }
+
+    function setDefaults() {
+        sessionLength.innerText = `${defaultSession}:00`;
+        breakLength.innerText = `${defaultBreak}:00`;
+    }
+
 
     useEffect(()=> {
         if(isActive === true) {
@@ -36,30 +71,69 @@ const SessionTimer = () => {
         }, [isActive]
     )
 
+    // SESSION EFFECT
     useEffect(()=>{
-        let interval = null;
-        if(isActive && nextSecond >= 10){
+        let interval = null; 
+        if(isActive && isBreak === false && nextSecond >= 10){
+            setDefaults();
             interval = setInterval(()=>{
                 setSessionSeconds(nextSecond);
                 timeLeft.innerText = `${nextMinute}:${nextSecond}`;
-            },1000);
+
+            },100);
         } else if(isActive && nextSecond < 10 && nextSecond > -1){
+            setDefaults();
             interval = setInterval(()=>{
-                console.log(nextMinute)
                 setSessionSeconds(nextSecond);
                 timeLeft.innerText = `${nextMinute}:0${nextSecond}`;
-            },1000);
+            },100);
         } else if(isActive && nextSecond === -1) {
+            setDefaults();
             console.log(nextSecond)
             if (nextMinute > 0 ) {
                 setSessionMinutes(nextMinute)
-                setSessionSeconds(nextSecond + 61);
-            } else {
+                setSessionSeconds(nextSecond + 60);
+            }
+            else {
                 console.log('Timer done!')
+                setTimerLabel("Break")
+                setIsBreak(true)
+                setSessionSeconds(sessionSeconds + 60)
+                setSessionMinutes(defaultSession)
+                timeLeft.innerText = `${breakMinutes}:00`
             }
         } 
         return () => clearInterval(interval);
-    }, [isActive, sessionSeconds]);
+    }, [isActive, isBreak, sessionSeconds]);
+
+    // BREAK EFFECT
+    useEffect(()=>{
+        let interval = null;
+        if(isActive && isBreak === true && nextBreakSecond >= 10){
+            interval = setInterval(()=>{
+                setBreakSeconds(nextBreakSecond);
+                timeLeft.innerText = `${nextBreakMinute}:${nextBreakSecond}`;
+            },100);
+        } else if(isActive && nextBreakSecond < 10 && nextBreakSecond > -1){
+            interval = setInterval(()=>{
+                setBreakSeconds(nextBreakSecond);
+                timeLeft.innerText = `${nextBreakMinute}:0${nextBreakSecond}`;
+            },100);
+        } else if(isActive && nextBreakSecond === -1) {
+            if (nextBreakMinute > 0 ) {
+                setBreakMinutes(nextBreakMinute)
+                setBreakSeconds(nextBreakSecond + 60);
+            } else {
+                console.log('Break timer done!');
+                setTimerLabel("Session");
+                setIsBreak(false);
+                setBreakSeconds(breakMinutes + 60)
+                setBreakMinutes(defaultBreak)
+                timeLeft.innerText = `${sessionMinutes}:00`
+            }
+        } 
+        return () => clearInterval(interval);
+    }, [isActive, isBreak, breakSeconds]);
 
 
     return(
@@ -74,6 +148,16 @@ const SessionTimer = () => {
                         <FontAwesomeIcon icon={faArrowDown} /></i></div>
                     </div>
             </div>
+            <div id="break-block">        
+                <p id="break-label">Break Length</p>
+                    <div id="break-counter">
+                    <div className="expand"><i id="break-increment" onClick={incrementBreakMinutes}>
+                        <FontAwesomeIcon icon={faArrowUp} /></i></div>
+                        <div id="break-length" className="expand">{breakMinutes}:00</div>
+                        <div className="expand"><i id="break-decrement" onClick={decrementBreakMinutes}>
+                        <FontAwesomeIcon icon={faArrowDown} /></i></div>
+                    </div>
+            </div>
             <div id="timer-border">
         <div id="timer-label">{timerLabel}</div>
         <div id="time-left">{sessionMinutes}:00
@@ -82,11 +166,7 @@ const SessionTimer = () => {
        </div>
         <div id="timer-options">
             <div id="start_stop"><i className="fa-solid fa-play" onClick={()=> {
-                toggle();
-                // setTimeout(()=>{
-                //     setSessionMinutes(nextMinute);
-                // },770);
-                
+                toggle();                
             }}>
                 {playPause}</i></div>
             <div id="reset"><i className="fas fa-undo"><FontAwesomeIcon icon={faUndo} /></i></div>
